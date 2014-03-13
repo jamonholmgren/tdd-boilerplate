cc11cc4ddd2a3b8575e3737ec655f48ec5817e78
Jamon Holmgren <jamon@clearsightstudio.com>
2014-03-12 22:39:31 -0700

    first mocha test and node suite

---
+++ test/mocha/suite.js
@@ -0,0 +1,18 @@
+// mocha/suite.js
+
+var boilerplate;
+var assert;
+
+if (typeof require == 'function') {
+  // enable to re-use in a browser without require.js
+  // boilerplate = 'boilerplate';
+  // assert = require('assert');
+  boilerplate = require('../../index.js');
+  assert = require('assert');
+}
+
+describe('smoke test', function() {
+  it('should pass', function () {
+    assert.equal(boilerplate(), 'boilerplate', 'failure message');
+  });
+});
